d630910277333e9ac2b693266230dca3b69ceb71
SevenLightnapper <dairenray@gmail.com>
2024-07-17 17:26:18 +0300

    hw4 complete. added .lower() command to disregard register when comparing words.

--- module3/hw4/module_3_4.py
+++ module3/hw4/module_3_4.py
@@ -22,13 +22,13 @@ def single_root_words(root_word, *other_words):
     same_words = []
 
     for word in other_words:
-        if root_word in word or word in root_word:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
             same_words.append(word)
 
     return same_words
 
 
 result = single_root_words('cat',
-                           'caterpillar', 'dog', 'scatter', 'catalog', 'bat', 'category', 'c', 'a', 't')
+                           'Caterpillar', 'dog', 'Scatter', 'catalog', 'bat', 'category', 'c', 'A', 't')
 
 print(result)
